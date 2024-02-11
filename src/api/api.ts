1ba9b24848d74b53b447acd0d4f731d62af142f0
tillherpertz <tillh@live.de>
2024-02-11 15:06:14 +0100

    fixes

--- src/api/api.ts
+++ src/api/api.ts
@@ -1,4 +1,5 @@
 import PocketBase from 'pocketbase';
+import ts from 'typescript';
 
 const apiUrl = import.meta.env.VITE_POCKETBASE_API_URL_DEV;
 const apiUser = import.meta.env.VITE_POCKETBASE_API_USER;
@@ -6,6 +7,7 @@ const apiPass = import.meta.env.VITE_POCKETBASE_API_PASS;
 
 const pb = new PocketBase(apiUrl);
 
+// @ts-ignore
 const authData = await pb.admins.authWithPassword(apiUser, apiPass);
 
 export default {
